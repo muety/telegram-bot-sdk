ae3c71ae3f3527d43e2612e0d0cba4d1a2f646f2
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2018-01-18 23:59:48 +0100

    Return 200 for webhook requests.

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ function _listen(port, path) {
         var result = req.body;
         if (result.message) _processMessage(_.assign(new Message, result.message, { chat_id: result.message.chat.id }));
         else if (result.inline_query && result.inline_query.query.length) _inlineQueryCallback(_.assign(new InlineQuery, result.inline_query));
-        else return res.status(400).end();
         return res.status(200).end();
     });
 
